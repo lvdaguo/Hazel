78eb5ed1c7390abbbea0a0b494db0c73b7574b42
lvdaguo <2227699871@qq.com>
2022-03-13 11:31:35 +0800

    Logging

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,6 +5,10 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initilized log!");
+	HZ_INFO("Initilized log!");
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
