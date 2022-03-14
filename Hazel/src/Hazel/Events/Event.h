495879e4399b54421459fa0744988b8a6e0d2f13
lvdaguo <2227699871@qq.com>
2022-03-14 10:23:04 +0800

    Event System

---
+++ Hazel/src/Hazel/Events/Event.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include "Hazel/Core.h"
+
+#include <string>
+#include <functional>
+
+namespace Hazel {
+
+	// 当前事件都为阻塞，当事件触发时，立刻执行订阅函数
+	// 更好的方法是将事件缓冲，在update的某一阶段一次性全部处理
+
+	enum class EventType
+	{
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	enum EventCategory
+	{
+		None = 0,
+		EventCategoryApplication	= BIT(0),
+		EventCategoryInput			= BIT(1),
+		EventCategoryKeyboard		= BIT(2),
+		EventCategoryMouse			= BIT(3),
+		EventCategoryMouseButton	= BIT(4),
+	};
+
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+								virtual EventType GetEventType() const override { return GetStaticType(); }\
+								virtual const char* GetName() const override { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+
+	class HAZEL_API Event
+	{
+		friend class EventDispatcher;
+	public:
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual int GetCategoryFlags() const = 0;
+		virtual std::string ToString() const { return GetName(); }
+
+		inline bool IsInCategory(EventCategory category)
+		{
+			return GetCategoryFlags() & category;
+		}
+
+	protected:
+		bool m_handled = false;
+	};
+
+	class EventDispatcher
+	{
+		template<typename T>
+		using EventFn = std::function<bool(T&)>;
+	public:
+		EventDispatcher(Event& event)
+			: m_event(event) { }
+
+		template <typename T>
+		bool Dispatch(EventFn<T> func)
+		{
+			if (m_event.GetEventType() == T::GetStaticType())
+			{
+				// 向下转换
+				m_Event.m_handled = func(*(T*)&m_event);
+				return true;
+			}
+			return false;
+		}
+	private:
+		Event& m_event;
+	};
+
+	inline std::ostream& operator << (std::ostream& os, const Event& e)
+	{
+		return os << e.ToString();
+	}
+}
