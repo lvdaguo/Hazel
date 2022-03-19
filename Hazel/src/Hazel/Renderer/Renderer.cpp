ac54347f0167061064b6e07c9ac4e2ef66fc18ca
lvdaguo <2227699871@qq.com>
2022-03-19 16:06:15 +0800

    Transforms

--- Hazel/src/Hazel/Renderer/Renderer.cpp
+++ Hazel/src/Hazel/Renderer/Renderer.cpp
@@ -13,10 +13,11 @@ namespace Hazel {
 	{
 	}
 
-	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray)
+	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
 		shader->UploadUniformMat4("u_ViewProjection", s_sceneData->ViewProjectionMatrix);
+		shader->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,5 +1,6 @@
 #include <Hazel.h>
 
+#include <glm/gtc/matrix_transform.hpp>
 #include <imgui/imgui.h>
 
 class ExampleLayer : public Hazel::Layer
@@ -35,10 +36,10 @@ public:
 
 		m_squareVA.reset(Hazel::VertexArray::Create());
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<Hazel::VertexBuffer> squareVB;
@@ -61,6 +62,7 @@ public:
 			layout(location = 1) in vec4 a_Color;			
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -69,7 +71,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -96,13 +98,13 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
-
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -145,7 +147,19 @@ public:
 
 		Hazel::Renderer::BeginScene(m_camera);
 
-		Hazel::Renderer::Submit(m_blueShader, m_squareVA);
+
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; ++y)
+		{
+			for (int x = 0; x < 20; ++x)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Hazel::Renderer::Submit(m_blueShader, m_squareVA, transform);
+			}
+		}
+
 		Hazel::Renderer::Submit(m_shader, m_vertexArray);
 
 		Hazel::Renderer::EndScene();
