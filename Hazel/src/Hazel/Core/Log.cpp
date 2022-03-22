b96eb126a93a301d274686a197581d8f28054fb3
lvdaguo <2227699871@qq.com>
2022-03-22 11:22:24 +0800

    fix intrumentor bug / Log message record to file

--- Hazel/src/Hazel/Core/Log.cpp
+++ Hazel/src/Hazel/Core/Log.cpp
@@ -1,6 +1,7 @@
 #include "hzpch.h"
 #include "Log.h"
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Hazel {
 	std::shared_ptr<spdlog::logger> Log::s_coreLogger;
@@ -8,11 +9,21 @@ namespace Hazel {
 
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^[%T] %n: %v%$");
-		s_coreLogger = spdlog::stdout_color_mt("HAZEL");
-		s_coreLogger->set_level(spdlog::level::trace);
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hazel.log", true));
 
-		s_clientLogger = spdlog::stdout_color_mt("APP");
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+		s_coreLogger = std::make_shared<spdlog::logger>("HAZEL", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_coreLogger);
+		s_coreLogger->set_level(spdlog::level::trace);
+		s_coreLogger->flush_on(spdlog::level::trace);
+
+		s_clientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_clientLogger);
 		s_clientLogger->set_level(spdlog::level::trace);
+		s_clientLogger->flush_on(spdlog::level::trace);
 	}
 }
